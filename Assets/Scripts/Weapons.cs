30d174aa65877076a4c5b708e85adcf434bd6b01
HadiChaaban <chaabanh@umich.edu>
2022-10-02 16:46:58 -0400

    Finished the 4 main Player mechanics
    Still need lots of polish but basic idea is complete

--- Assets/Scripts/Weapons.cs
+++ Assets/Scripts/Weapons.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Tilemaps;
 
 public class Weapons : MonoBehaviour
 {
@@ -9,13 +10,19 @@ public class Weapons : MonoBehaviour
     private Vector2 direction;
     public GameObject bullet;
     public Transform weaponPosition;
+    public GameObject shield;
     private bool isFacingRight;
     private string action = "";
     [SerializeField] private LayerMask enemyLayer;
     [SerializeField] private LayerMask groundLayer;
+    public Tile bridge;
+    public Tilemap floor;
     public float attackRange;
     public GameObject txt;
     private int option;
+    private float timer;
+    private Tile oldTile;
+    private Vector3Int currentCell;
 
     private void Awake(){
         playerControls = new PlayerControls();
@@ -34,51 +41,60 @@ public class Weapons : MonoBehaviour
         direction = playerControls.Player.Choice.ReadValue<Vector2>();
         float dirX = direction.x;
         float dirY = direction.y;
-        if ((dirX > -0.3f && dirX < 0.3f) && (dirY > 0.7f && dirY < 1f) && option != 1){
+        if ((dirX > -0.3f && dirX < 0.3f) && (dirY > 0.7f && dirY <= 1f) && option != 1){
             txt.GetComponent<TMPro.TextMeshProUGUI>().text = "BULLET";
             action = "bullet";
-        } else if ((dirX > 0.7f && dirX < 1f) && (dirY > -0.3f && dirY < 0.3f) && option != 2){
+        } else if ((dirX > 0.7f && dirX <= 1f) && (dirY > -0.3f && dirY < 0.3f) && option != 2){
             txt.GetComponent<TMPro.TextMeshProUGUI>().text = "SWORD";
             action = "sword";
-        } else if ((dirX > -0.3f && dirX < 0.3f) && (dirY > -1f && dirY < -0.7f) && option != 3){
+        } else if ((dirX > -0.3f && dirX < 0.3f) && (dirY >= -1f && dirY < -0.7f) && option != 3){
             txt.GetComponent<TMPro.TextMeshProUGUI>().text = "SHIELD";
             action = "shield";
-        } else if ((dirX > -1f && dirX < -0.7f) && (dirY > -0.3f && dirY < 0.3f) && option != 4){
+        } else if ((dirX >= -1f && dirX < -0.7f) && (dirY > -0.3f && dirY < 0.3f) && option != 4){
             txt.GetComponent<TMPro.TextMeshProUGUI>().text = "BRIDGE";
             action = "bridge";
         }
         bool temp = playerControls.Player.Fire.IsPressed();
-        if (temp){
+        if (temp && timer <= 0){
             switch (action){
                 case "bullet":
                     shootBullet();
+                    timer = 0.5f;
                     break;
                 case "sword":
                     useSword();
+                    timer = 0.5f;
                     break;
                 case "shield":
                     useShield();
+                    timer = 0.5f;
                     break;
                 case "bridge":
                     placeBridge();
+                    timer = 0.5f;
                     break;
             }
         }
+        timer -= Time.deltaTime;
     }
     void shootBullet(){
+        shield.SetActive(false);
+        floor.SetTile(currentCell, oldTile);
         GameObject bulletInstance = Instantiate(bullet, weaponPosition.position, transform.rotation);
         if(isFacingRight){
-            bulletInstance.GetComponent<Rigidbody2D>().velocity = transform.right * 5f;
+            bulletInstance.GetComponent<Rigidbody2D>().velocity = transform.right * 20f;
         } else {
-            bulletInstance.GetComponent<Rigidbody2D>().velocity = transform.right * -5f;
+            bulletInstance.GetComponent<Rigidbody2D>().velocity = transform.right * -20f;
         }
         action = "";
         option = 1;
     }
 
     void useSword(){
+        shield.SetActive(false);
+        floor.SetTile(currentCell, oldTile);
         Collider2D coll = Physics2D.OverlapCircle(weaponPosition.position, attackRange, enemyLayer);
-        RaycastHit2D right = Physics2D.Raycast(transform.position, /*weaponPosition.position*/transform.TransformDirection(Vector2.right), attackRange, groundLayer);
+        RaycastHit2D right = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector2.right), attackRange, groundLayer);
         if(coll && !right){
             Destroy(coll.gameObject);
         }
@@ -87,11 +103,21 @@ public class Weapons : MonoBehaviour
     }
 
     void useShield(){
+        shield.SetActive(true);
+        floor.SetTile(currentCell, oldTile);
         action = "";
         option = 3;
     }
 
     void placeBridge(){
+        shield.SetActive(false);
+        currentCell = floor.WorldToCell(transform.position);
+        currentCell.y -= 2;
+        currentCell.x += isFacingRight ? 1 : -1;
+        oldTile = (Tile)floor.GetTile(currentCell);
+        if (floor.GetTile(currentCell) == null){
+            floor.SetTile(currentCell, bridge);
+        }
         action = "";
         option = 4;
     }
